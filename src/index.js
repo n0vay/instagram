02216b31fe5781408eae73bf64bbb3f611970f0d
n0vay <koshantanu@gmail.com>
2022-11-02 17:10:04 -0400

    firebase

--- src/index.js
+++ src/index.js
@@ -4,15 +4,3 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// Client side rendering
-
-//folder structure
-//src
-// -> Components
-// -> Constants
-// -> Context
-// -> helpers
-// -> hooks
-// -> Lib (Firebase)
-// -> Services
-// -> Styles (Tailwind)
