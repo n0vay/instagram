8740a1c868320d0e55c1ab03bd5291758a035f60
n0vay <koshantanu@gmail.com>
2022-11-11 01:22:59 -0500

    fixed-almost

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import FirebaseContext from "./context/firebase";
 import { firebase, FieldValue } from "./lib/firebase";
-
-// ReactDOM.render(
-//   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-//     <App />
-//   </FirebaseContext.Provider>,
-//   document.getElementById("root")
-// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </FirebaseContext.Provider>
 );
-
-reportWebVitals();
